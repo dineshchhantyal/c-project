132e928a7aad8b2eacc1424b5282fe6cf17c5d88
Dinesh Chhantyal <myagdichhantyal@gmail.com>
2021-08-07 16:05:47 +0545

    NAME - fix

--- main.c
+++ main.c
@@ -6,6 +6,6 @@
 void main(){
     printf("%d",sum(1,2));
     readFile("./files/controller/leaderControl.txt");
-    clear("../files/controller/leaderControl.txt", "../files/leaders/"); // clear the file BASED ON FILE NAME OF CONTROLLER
+    clear("./files/controller/leaderControl.txt", "./files/leaders/"); // clear the file BASED ON FILE NAME OF CONTROLLER
 }
 
