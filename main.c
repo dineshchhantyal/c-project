fadd0d29701e7f2da7badf6b76a556a64684f080
Dinesh Chhantyal <myagdichhantyal@gmail.com>
2021-08-09 08:22:19 +0545

    clear chnaged to reset

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 #include "./header/common.h"
 #include "./header/fileHandeling.h"
-#include "./components/clear.c"
+#include "./components/reset.c"
 
 void main(){
     printf("%d",sum(1,2));
     readFile("./files/controller/leaderControl.txt");
-    clear("./files/controller/leaderControl.txt", "./files/leaders/"); // clear the file BASED ON FILE NAME OF CONTROLLER
+    reset("./files/controller/leaderControl.txt", "./files/leaders/"); // reset the file BASED ON FILE NAME OF CONTROLLER
 }
 
