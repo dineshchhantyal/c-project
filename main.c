8306452b476dd8e90d468dcc88bb2957cd86e5a6
Lokesh <lokeshpan31@gmail.com>
2021-08-10 08:43:05 +0545

    Added

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
 #include "./header/common.h"
 #include "./header/fileHandeling.h"
+#include "./shift.c"
 
 void main(){
     printf("%d",sum(1,2));
     readFile("./code.txt");
+    shift();
 }
+
---
+++ shift.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+
+     int shift(){
+          int n;
+          printf("  Enter your choice (1, 2, 3, 4): ");  
+          scanf("%d",&n);
+
+return 0;
+}
