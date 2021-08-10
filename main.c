68d3f9fe7a9e384ba9940910e8658ce28e66aa21
Lokesh <lokeshpan31@gmail.com>
2021-08-10 11:59:05 +0545

    Code Added

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 
 void main(){
     printf("%d",sum(1,2));
-    readFile("./code.txt");
+      readFile("./code.txt");
     shift();
 }
 
--- shift.c
+++ shift.c
@@ -1,10 +1,37 @@
 #include<stdio.h>
 #include<conio.h>
 
+ 
      int shift(){
+          
+          FILE *fp;
           int n;
+         first:
           printf("  Enter your choice (1, 2, 3, 4): ");  
           scanf("%d",&n);
-
+   if(n==1)
+    fp=fopen("./files/components/AboutLeaders.txt","r");
+    int ch,num;
+    while((ch=fgetc(fp))!=EOF)
+     {
+          printf("%c",ch);
+     
+     }
+     printf("\nEnter number(1, 2, 3, 4, 5): ");
+     scanf("%d",num);
+   if(num==5)
+   goto first;
+      if(n==2)
+    fp=fopen("./files/components/Cast.txt","r");
+    while((ch=fgetc(fp))!=EOF)
+     {
+          printf("%c",ch);
+     }
+      if(n==3)
+    fp=fopen("./files/components/Result.txt","r");
+    while((ch=fgetc(fp))!=EOF)
+     {
+          printf("%c",ch);
+     }
 return 0;
 }
