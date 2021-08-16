7dd540f299cfbad4dec0bbeaa6b58e559cff9e37
Manish-Kunwar <kunwarmanish378@gmail.com>
2021-08-16 12:52:05 +0545

    Result code simplified.

--- components/result.c
+++ components/result.c
@@ -1,85 +1,46 @@
-int shift();
-
 int result(){
-    FILE *result;
-    int r1,r2,r3,r4, yesNo;
+    FILE *result,*fp;
+    char leaders[50],name[50];
+    int i=0,r[4],win,line;
+    printf(" ----------------------Result!!!!-----------------------\n");
 
-    printf("\t\t    Result!!!!!");
-    
-    result=fopen("./files/votes/Baburam Bhattarai.txt","r");
-       if (result==NULL){
-           printf("\n\n FIle can't be open.");
-           exit(1);
-       }
-       if (fscanf(result,"%d",&r1)==EOF){
-           printf("\n\n NO votes are casted for Baburam Bhattarai.");
-           r1=0;
-       }
-       else{
-           while (fscanf(result,"%d",&r1) !=EOF);
-           printf("\n\n The number of vote obtained by Baburam Bhattarai is %d.",r1);
-       }
+    result=fopen("./files/leaderNames.txt","r");
+        while(fscanf(result," %[^\n]",leaders)!=EOF){
+            char filename[50]="./files/votes/",ext[5]=".txt";
+            strcpy(name,leaders);
+            strcat(leaders,ext);
+            strcat(filename,leaders);
+            fp=fopen(filename,"r");
+                if (result==NULL){
+                    printf("\n\n FIle can't be open.");
+                    exit(1);
+                }
+                else if (fscanf(fp,"%d",&r[i])==EOF){
+                    printf("\n\n No votes are casted for %s.",name);
+                    r[i]=0;
+                }
+                else{
+                    fscanf(fp,"%d",&r[i]);
+                    printf("\n\n The number of vote obtained by %s is %d.",name,r[i]);
+                }
+            fclose(fp);
+            i++;
+        } 
+        win=r[0];
+        for (i = 0; i < 4; i++){
+            if (win<r[i]){
+                win=r[i];
+                line=i;
+            }
+        }
     fclose(result);
 
-    result=fopen("./files/votes/KP Oli.txt","r");
-       if (result==NULL){
-           printf("\n FIle can't be open.");
-           exit(1);
-       }
-       if (fscanf(result,"%d",&r2)==EOF){
-           printf("\n NO votes are casted for KP Oli.");
-           r2=0;
-       }
-       else{
-           while (fscanf(result,"%d",&r2) !=EOF);
-           printf("\n The number of vote obtained by KP Oli is %d.",r2);
-       }
+    result=fopen("./files/leaderNames.txt","r");
+        for (i=0;fscanf(result," %[^\n]",leaders)!=EOF;i++){
+            if (i==line)
+                printf("\n\n\n\t %s won the election.",leaders);
+        }
     fclose(result);
 
-    result=fopen("./files/votes/Puspa Kamal Dahal.txt","r");
-       if (result==NULL){
-           printf("\n FIle can't be open.");
-           exit(1);
-       }
-       if (fscanf(result,"%d",&r3)==EOF){
-           printf("\n NO votes are casted for Pushpa Kamal Dahal.");
-           r3=0;
-       }
-       else{
-           while (fscanf(result,"%d",&r3) !=EOF);
-           printf("\n The number of vote obtained by Pushpa Kamal Dahal is %d.",r3);
-       }
-    fclose(result);
-
-    result=fopen("./files/votes/Sher Bahadur Deuba.txt","r");
-       if (result==NULL){
-           printf("\n FIle can't be open.");
-           exit(1);
-       }
-       if (fscanf(result,"%d",&r4)==EOF){
-           printf("\n NO votes are casted for Sher Bahadur Deuba.");
-           r4=0;
-       }
-       else{
-           while (fscanf(result,"%d",&r4) !=EOF);
-           printf("\n The number of vote obtained by Sher Bahadur Deuba is %d.",r4);
-       }
-    fclose(result);
-    
-    if (r1>r2 && r1>r3 && r1>r4)
-        printf("\n\n\t    Baburam Bhattarai won the election.");
-    else if (r2>r1 && r2>r3 && r2>r4)
-        printf("\n\n\t    KP Oli won the election.");
-    else if (r3>r1 && r3>r2 && r3>r4)
-        printf("\n\n\t    PUSHPA Kamal Dahal won the election.");
-    else if (r4>r1 && r4>r2 && r4>r3)
-        printf("\n\n\t    Sher Bahadur Deuba won the election."); 
-    else
-        printf("\n\n\t\tThe election is draw.");
-    
-    printf("\n \n \n  Back To Home ? \n [Y] : Yes \t [N] : No \n : -> ");
-    scanf(" %c", &yesNo);
-    if (checkYesNo(yesNo) == 0) shift();
-
-    return 0;  
+    return 0;   
 }
